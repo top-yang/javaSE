e7cc8968412954a9524592470107c6d8efaeecd4
yanghao <top.tayloryang@gmail.com>
2020-11-07 21:26:02 +0800

    网络编程完结

--- src/cn/hao/net/practice/udp/boys.java
+++ src/cn/hao/net/practice/udp/boys.java
@@ -1,7 +1,7 @@
 package cn.hao.net.practice.udp;
 
 import java.net.SocketException;
-
+//commit 网络编程
 public class boys {
     public static void main(String[] args) throws SocketException {
         new Thread(new Send(9990,9998,"localhost")).start();
